861c813cf9f7a779a8300e41eb239a1dd548607c
Dwalker1000 <25walkerd@gmail.com>
2022-01-13 17:35:22 -0600

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autons/BlueNearAutonNoCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autons/BlueNearAutonNoCamera.java
@@ -4,9 +4,13 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 import org.firstinspires.ftc.teamcode.CompBotV3.CompBotV3Attachments;
 import org.firstinspires.ftc.teamcode.CompBotW1.CompBotW1Attachments;
+
+import static org.firstinspires.ftc.teamcode.CompBotV3.CompBotV3.driveUntilMechStop;
+import static org.firstinspires.ftc.teamcode.CompBotV3.CompBotV3.nEncDrive;
+import static org.firstinspires.ftc.teamcode.CompBotV3.CompBotV3.runMotorTime;
+import java.util.Arrays;
 
 // Start blue storage side
 
@@ -24,13 +28,13 @@ public class BlueNearAutonNoCamera extends LinearOpMode {
         telemetry.update();
         boolean[] pos = {false,false,false};
         ElapsedTime e = new ElapsedTime();
-//        while(!isStarted()) {
-//            pos = r.p.getPositions();
-//        }
+        while(!isStarted()) {
+            pos = r.p.getPositions();
+        }
 
-//        r.phoneCam.stopStreaming();
+        r.phoneCam.stopStreaming();
 
-//        runtime.reset();
+        runtime.reset();
 
         r.setBucket(1);
 
@@ -38,42 +42,42 @@ public class BlueNearAutonNoCamera extends LinearOpMode {
         r.AEncDrive(0,30,0,dPower);
 
         // lift and drop
-//        if (Arrays.equals(pos, new boolean[]{true, false, false})) {// left
-//            r.AEncDrive(20,0,dPower,0);
-//            nEncDrive(r.lift,1300,1);
-//            runMotorTime(r.bucket,-1,1000);
-//            sleep(2000);
-//            r.bucket.setPower(1);
-//            driveUntilMechStop(r.lift,-1, 1000);
-//            r.AEncDrive(-14,0,-dPower,0);
-//        } else if (Arrays.equals(pos, new boolean[]{false, true, false})) {// middle
-//            r.AEncDrive(16,0,dPower,0);
-//            nEncDrive(r.lift,3000,1);
-//            runMotorTime(r.bucket,-1,1000);
-//            sleep(2000);
-//            r.bucket.setPower(1);
-//            driveUntilMechStop(r.lift,-1, 1000);
-//            r.AEncDrive(-10,0,-dPower,0);
-//        } else {// right
-//            r.AEncDrive(10,0,dPower,0);
-//            driveUntilMechStop(r.lift,1, 1000);
-//            runMotorTime(r.bucket,-1,1000);
-//            sleep(2000);
-//            r.bucket.setPower(1);
-//            driveUntilMechStop(r.lift,-1, 1000);
-//            r.AEncDrive(-4,0,-dPower,0);
-//        }
-//        telemetry.addLine("finished with lift");
-//        telemetry.update();
-//        r.bucket.setPower(0);
+        if (Arrays.equals(pos, new boolean[]{true, false, false})) {// left
+            r.AEncDrive(20,0,dPower,0);
+            nEncDrive(r.lift,1300,1);
+            r.setBucket(1);
+            sleep(2000);
+            r.setBucket(1);
+            driveUntilMechStop(r.lift,-1, 1000);
+            r.AEncDrive(-14,0,-dPower,0);
+        } else if (Arrays.equals(pos, new boolean[]{false, true, false})) {// middle
+            r.AEncDrive(16,0,dPower,0);
+            nEncDrive(r.lift,3000,1);
+            r.setBucket(1);
+            sleep(2000);
+            r.setBucket(1);
+            driveUntilMechStop(r.lift,-1, 1000);
+            r.AEncDrive(-10,0,-dPower,0);
+        } else {// right
+            r.AEncDrive(10,0,dPower,0);
+            driveUntilMechStop(r.lift,1, 1000);
+            r.setBucket(1);
+            sleep(2000);
+            r.setBucket(1);
+            driveUntilMechStop(r.lift,-1, 1000);
+            r.AEncDrive(-4,0,-dPower,0);
+        }
+        telemetry.addLine("finished with lift");
+        telemetry.update();
+        r.setBucket(0);
 
 
         // Strafe to warehouse
         r.gyroTurn(90,0.2);
         telemetry.addLine("finished with turn");
         telemetry.update();
-//        r.AEncDrive(0,-10,0,-dPower, 3000);
-//        r.AEncDrive(0,-15,0,-0.15,3000);
+        r.AEncDrive(0,-10,0,-dPower, 3000);
+        r.AEncDrive(0,-15,0,-0.15,3000);
         r.AEncDrive(100,0,1,0);
 
         r.stop();
